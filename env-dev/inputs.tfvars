b609b9e63d2de00d19a5a6b14aceea5a05896b3e
Wale <Adewale.megbabi@gmail.com>
2023-10-02 13:14:35 +0100

    Initial commit

--- env-dev/inputs.tfvars
+++ env-dev/inputs.tfvars
@@ -4,3 +4,5 @@ vpc_cidr = "10.0.0.0/16"
 public_subnets = ["10.0.0.0/24", "10.0.1.0/24"]
 private_subnets = ["10.0.2.0/24", "10.0.3.0/24"]
 azs = ["us-east-1a", "us-east-1b"]
+default_vpc_id = "vpc-0e1835f8eaa25eb69"
+account_no = "0080-8940-8493"
--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -50,6 +50,10 @@ resource "aws_vpc_peering_connection" "peering" {
   peer_owner_id = var.account_no
   peer_vpc_id   = var.default_vpc_id
   vpc_id        = aws_vpc.main.id
+  auto_accept   = true
+  tags = {
+    Name = "peering-from-default-vpc-to ${var.env}-vpc"
+  }
 }
 
 resource "aws_route_table" "public" {
