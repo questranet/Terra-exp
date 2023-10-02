3993c5167785636a754e2470575ad4529805d9bc
Wale <Adewale.megbabi@gmail.com>
2023-10-02 14:00:26 +0100

    Initial commit

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -78,7 +78,7 @@ resource "aws_route_table" "private" {
   }
 
   route {
-    cidr_block = "var.default_vpc_cidr"
+    cidr_block = var.default_vpc_cidr
     vpc_peering_connection_id = "aws_vpc_peering_connection.peering.id"
   }
 
