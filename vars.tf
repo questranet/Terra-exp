70c354e87f4ffb053822b267c7939e7dae13bb3c
Wale <Adewale.megbabi@gmail.com>
2023-10-02 21:06:21 +0100

    Initial commit

--- vars.tf
+++ vars.tf
@@ -3,7 +3,7 @@ variable "vpc_cidr" {}
 variable "public_subnets" {}
 variable "private_subnets" {}
 variable "azs" {}
-variable "account_no" {}
 variable "default_vpc_id" {}
+variable "account_no" {}
 variable "default_vpc_cidr" {}
 variable "default_route_table_id" {}
