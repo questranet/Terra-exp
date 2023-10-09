e169ec308a4ece932930490fa44bbf91c28e6141
Wale <Adewale.megbabi@gmail.com>
2023-10-09 10:06:55 +0100

    Initial commit

--- modules/app/main.tf
+++ modules/app/main.tf
@@ -28,9 +28,11 @@ resource "aws_launch_template" "template" {
   image_id               = data.aws_ami.ami.id
   instance_type          = var.instance_type
   vpc_security_group_ids = [aws_security_group.security_group.id]
+  user_data              = filebase64(templatefile("${path.module}/userdata.sh"), {
+  role_name              = var.component
+  })
 
-
-  tag_specifications {
+tag_specifications {
     resource_type = "instance"
     tags = {
       Name = "${var.env}-${var.component}"
