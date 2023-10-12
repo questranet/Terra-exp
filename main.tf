07ec169a0af6e7a05b6dc1ccdba95bcaa9d770ea
Wale <Adewale.megbabi@gmail.com>
2023-10-13 00:30:07 +0100

    Initial commit

--- main.tf
+++ main.tf
@@ -29,6 +29,8 @@ module "private-lb" {
   internal          = true
   subnets           = module.vpc.private_subnets
   vpc_id            = module.vpc.vpc_id
+  dns_name          = "backend-${var.env}.waleapagun.online"
+  zone_id           = "Z08999912AI7EUJ47AGDO"
 }
 #
 #module "frontend" {
