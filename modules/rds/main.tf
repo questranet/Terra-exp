12096f073a6a70bdc2fe1c35dd1b45b536e3df8f
Wale <Adewale.megbabi@gmail.com>
2023-10-12 22:04:31 +0100

    Initial commit

--- modules/rds/main.tf
+++ modules/rds/main.tf
@@ -35,7 +35,7 @@ resource "aws_db_subnet_group" "main" {
 resource "aws_rds_cluster" "main" {
   cluster_identifier      = "${var.env}-${var.component}"
   engine_version          = "5.7.mysql_aurora.2.11.3"
-  db_subnet_group_name    = "aws_db_subnet_group.main.name"
+  db_subnet_group_name    =  aws_db_subnet_group.main.name
   database_name           = "dummy"
   master_username         = "data.aws_ssm_parameter.master_username.value"
   master_password         = "data.aws_ssm_parameter.master_password.value"
