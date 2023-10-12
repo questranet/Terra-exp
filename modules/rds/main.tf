cabc95c56ae0fd2ca993d1be2610bd9a274e9f31
Wale <Adewale.megbabi@gmail.com>
2023-10-12 22:35:26 +0100

    Initial commit

--- modules/rds/main.tf
+++ modules/rds/main.tf
@@ -42,5 +42,13 @@ resource "aws_rds_cluster" "main" {
   master_password         = data.aws_ssm_parameter.master_password.value
   vpc_security_group_ids  = [aws_security_group.main.id]
   skip_final_snapshot     = true
+}
 
+resource "aws_rds_cluster_instance" "cluster_instances" {
+  count              = 1
+  identifier         = "aurora-cluster-demo-${count.index}-instance-${count.index}"
+  cluster_identifier = aws_rds_cluster.main.id
+  instance_class     = "db.t3.medium"
+  engine             = aws_rds_cluster.main.engine
+  engine_version     = aws_rds_cluster.main.engine_version
 }
