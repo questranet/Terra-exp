8eca36906bd2d66b512049d646f8197bc77c4118
Wale <Adewale.megbabi@gmail.com>
2023-10-13 09:15:25 +0100

    Initial commit

--- main.tf
+++ main.tf
@@ -11,15 +11,18 @@ module "vpc" {
   default_route_table_id = var.default_route_table_id
 }
 
-#module "public-lb" {
-#  source            = "./modules/alb"
-#  alb_sg_allow_cidr = "0.0.0.0/0"
-#  alb_type          = "public"
-#  env               = var.env
-#  internal          = false
-#  subnets           = module.vpc.public_subnets
-#  vpc_id            = module.vpc.vpc_id
-#}
+module "public-lb" {
+  source            = "./modules/alb"
+  alb_sg_allow_cidr = "0.0.0.0/0"
+  alb_type          = "public"
+  env               = var.env
+  internal          = false
+  subnets           = module.vpc.public_subnets
+  vpc_id            = module.vpc.vpc_id
+  dns_name          = "${var.env}.waleapagun.online"
+  zone_id           = "Z08999912AI7EUJ47AGDO"
+  tg_arn            = module.frontend.tg_arn
+}
 
 module "private-lb" {
   source            = "./modules/alb"
