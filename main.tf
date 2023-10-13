68c1e992be0e7139e599c82f81acec06681a94f1
Wale <Adewale.megbabi@gmail.com>
2023-10-13 17:51:36 +0100

    Initial commit

--- main.tf
+++ main.tf
@@ -36,37 +36,37 @@ module "private-lb" {
   zone_id           = "Z08999912AI7EUJ47AGDO"
   tg_arn            = module.backend.tg_arn
 }
-
-module "frontend" {
-  source = "./modules/app"
-  app_port               = 80
-  component              = "frontend"
-  env                    = var.env
-  instance_type          = "t3.micro"
-  vpc_cidr               = var.vpc_cidr
-  vpc_id                 = module.vpc.vpc_id
-  subnets                = module.vpc.private_subnets
-  bastion_node_cidr      = var.bastion_node_cidr
-  desired_capacity       = var.desired_capacity
-  max_size               = var.max_size
-  min_size               = var.min_size
-}
-
-module "backend" {
-  depends_on             = [module.mysql]
-  source                 = "./modules/app"
-  app_port               = 8080
-  component              = "backend"
-  env                    = var.env
-  instance_type          = "t3.micro"
-  vpc_cidr               = var.vpc_cidr
-  vpc_id                 = module.vpc.vpc_id
-  subnets                = module.vpc.private_subnets
-  bastion_node_cidr      = var.bastion_node_cidr
-  desired_capacity       = var.desired_capacity
-  max_size               = var.max_size
-  min_size               = var.min_size
-}
+#
+#module "frontend" {
+#  source = "./modules/app"
+#  app_port               = 80
+#  component              = "frontend"
+#  env                    = var.env
+#  instance_type          = "t3.micro"
+#  vpc_cidr               = var.vpc_cidr
+#  vpc_id                 = module.vpc.vpc_id
+#  subnets                = module.vpc.private_subnets
+#  bastion_node_cidr      = var.bastion_node_cidr
+#  desired_capacity       = var.desired_capacity
+#  max_size               = var.max_size
+#  min_size               = var.min_size
+#}
+#
+#module "backend" {
+#  depends_on             = [module.mysql]
+#  source                 = "./modules/app"
+#  app_port               = 8080
+#  component              = "backend"
+#  env                    = var.env
+#  instance_type          = "t3.micro"
+#  vpc_cidr               = var.vpc_cidr
+#  vpc_id                 = module.vpc.vpc_id
+#  subnets                = module.vpc.private_subnets
+#  bastion_node_cidr      = var.bastion_node_cidr
+#  desired_capacity       = var.desired_capacity
+#  max_size               = var.max_size
+#  min_size               = var.min_size
+#}
 
 module "mysql" {
   source = "./modules/rds"
