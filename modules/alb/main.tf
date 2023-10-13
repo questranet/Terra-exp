06f9aa5387668bb0955d4ecd3bbba005efe71640
Wale <Adewale.megbabi@gmail.com>
2023-10-13 15:10:57 +0100

    Initial commit

--- modules/alb/main.tf
+++ modules/alb/main.tf
@@ -50,18 +50,36 @@ resource "aws_route53_record" "www" {
   records = [aws_lb.alb.dns_name]
 }
 
-resource "aws_lb_listener" "listener-http" {
-  load_balancer_arn = aws_lb.alb.arn
-  port              = "80"
-  protocol          = "HTTP"
+resource "aws_lb_listener" "listener-http-public" {
+  count                      = var.alb_type == "public" ? 1 : 0
+  load_balancer_arn          = aws_lb.alb.arn
+  port                       = "80"
+  protocol                   = "HTTP"
 
   default_action {
-    type = "forward"
-    target_group_arn = var.tg_arn
+    type = "redirect"
+
+    redirect {
+      port        = "443"
+      protocol    = "HTTPS"
+      status_code = "HTTP_301"
+    }
   }
 }
 
-resource "aws_lb_listener" "listener-https"{
+resource "aws_lb_listener" "listener-http-private" {
+  count                      = var.alb_type == "private" ? 1 : 0
+  load_balancer_arn          = aws_lb.alb.arn
+  port                       = "80"
+  protocol                   = "HTTP"
+
+  default_action {
+    type                     = "forward"
+    target_group_arn         = var.tg_arn
+  }
+}
+
+resource "aws_lb_listener" "listener-https-public"{
   count                     = var.alb_type == "public" ? 1 : 0
   load_balancer_arn         = aws_lb.alb.arn
   port                      = "443"
